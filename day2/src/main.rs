3340e10691d89fc3f142f72130e836cd4b4bb45a
Ken Aiken <ken@hypack.com>
2022-12-17 16:18:35 -0500

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::vec::Vec;
 
 static FILENAME : &'static str = "input.txt";
 fn main() {
@@ -15,33 +14,34 @@ fn main() {
     while len>2
     {
         let theirs = line.chars().nth(0).expect("err");
-        let mut mine = line.chars().nth(2).expect("err");
-        mine = std::char::from_u32( mine as u32 - 23).expect("err");
-
+        let result = line.chars().nth(2).expect("err");
+        let mut shift;
+        
+        match result{
+            'X'=>{ //lose
+                shift = -1i32;
+                totalscore += 0; 
+            },
+            'Y'=>{//draw
+                shift = 0;
+                totalscore += 3; 
+            },
+            'Z'=>{//win
+                shift = 1;
+                totalscore += 6; 
+            },
+            _=>panic!("Invalid input {:?}",result)
+        }
+        shift = theirs as i32 + shift;
+        if shift< ('A' as i32) { shift += 3; }
+        if shift> ('C' as i32)  { shift -= 3; }
+        let mine = std::char::from_u32( shift as u32 ).expect("err");
         match mine{
             'A'=>totalscore += 1, //rock
             'B'=>totalscore += 2, //paper
             'C'=>totalscore += 3, //sissors
             _=>panic!("Invalid input {:?}",mine)
         }
-
-        if theirs == mine //draw
-        {
-            totalscore += 3;
-        }
-        else if (  //I win
-            ( (theirs ==('A')) && (mine == ('B') )) ||
-            ( (theirs ==('B')) && (mine == ('C') )) ||
-            ( (theirs ==('C')) && (mine == ('A') )) 
-        )
-        {
-            totalscore += 6;
-        }
-        else // I win
-        {
-            totalscore += 0;
-        }
-
         
         //prep next line
         line = String::new();
