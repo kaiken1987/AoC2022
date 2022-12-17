7c12e71e2742005344b296c151f92e3022350887
Ken Aiken <ken@hypack.com>
2022-12-17 16:23:13 -0500

    Preping future days

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+static FILENAME : &'static str = "testinput.txt";
 fn main() {
-    println!("Hello, world!");
+    println!("Reading File: {FILENAME}\n");
+    let file = File::open(FILENAME).expect("Unable to open {FILENAME}");
+    let mut reader = io::BufReader::new(file);//
+    
+    let mut line = String::new();
+    let mut len = reader.read_line(&mut line).expect("Unable to read {FILENAME}");
+
+    while len>2
+    {
+        
+        //prep next line
+        line = String::new();
+        len = reader.read_line(&mut line).expect("Unable to read {FILENAME}");
+    }
+    //println!("Total Score: {totalscore}\n");
 }
