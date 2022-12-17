3b224d606e802c74701fdab2a465706c5b9a0b02
Ken Aiken <ken@hypack.com>
2022-12-17 17:11:33 -0500

    Day 3 Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,35 +7,40 @@ fn main() {
     let file = File::open(FILENAME).expect("Unable to open {FILENAME}");
     let mut reader = io::BufReader::new(file);//
     
-    let mut line = String::new();
-    let mut len = reader.read_line(&mut line).expect("Unable to read {FILENAME}");
+    let mut elfa = String::new();
+    let mut elfb = String::new();
+    let mut elfc = String::new();
+    let mut len = reader.read_line(&mut elfa).expect("Unable to read {FILENAME}");
+    if len>2 {len = reader.read_line(&mut elfb).expect("Unable to read {FILENAME}");}
+    if len>2 {len = reader.read_line(&mut elfc).expect("Unable to read {FILENAME}");}
 
-    let mut totalPriority = 0;
+    let mut total_priority = 0;
     while len>2
     {
-        let trim = line.trim();
-        let at = trim.len()/2;
-        let (compa, compb) = trim.split_at(at);
         //println!( "{compa}    {compb}");
-        let common = findCommon(compa.to_string(),compb.to_string());
-        if common != '0' {
+        let common = find_common3(elfa.to_string(),elfb.to_string(),elfc.to_string());
+        if common != '\r' {
             let pri : i32;
             match common {
                 'a'..='z' => {pri = common as i32 - ('a' as i32) + 1},
                 'A'..='Z' => {pri = common as i32 - ('A' as i32) + 27},
                 _=> panic!("err"),
             };
-            //println!( "common item is {common}, Priority {pri}");
-            totalPriority += pri;
+            println!( "common item is {common}, Priority {pri}");
+            total_priority += pri;
         }
         //prep next line
-        line = String::new();
-        len = reader.read_line(&mut line).expect("Unable to read {FILENAME}");
+        elfa = String::new();
+        elfb = String::new();
+        elfc = String::new();
+        len = reader.read_line(&mut elfa).expect("Unable to read {FILENAME}");
+        if len>2 {len = reader.read_line(&mut elfb).expect("Unable to read {FILENAME}");}
+        if len>2 {len = reader.read_line(&mut elfc).expect("Unable to read {FILENAME}");}
     }
-    println!("Total priority: {totalPriority}\n");
+    println!("Total priority: {total_priority}\n");
 }
 
-fn findCommon( astr:String, bstr:String ) -> char{
+fn find_common( astr:String, bstr:String ) -> char{
     for ac in astr.as_bytes().iter() {
         for bc in bstr.as_bytes().iter(){
             if  *ac == *bc{
@@ -44,4 +49,18 @@ fn findCommon( astr:String, bstr:String ) -> char{
         }
     }    
     return '0';
+}
+fn find_common3( astr:String, bstr:String, cstr:String ) -> char{
+    for ac in astr.as_bytes().iter() {
+        for bc in bstr.as_bytes().iter(){
+            if  *ac == *bc{
+                for cc in cstr.as_bytes().iter(){
+                    if  *ac == *cc{
+                        return std::char::from_u32( *ac as u32 ).expect("err");
+                    }
+                }
+            }
+        }
+    }    
+    return '\r';
 }
