892e7a00ee94b2798b4fbf7f383ce97e286354fc
Ken Aiken <ken@hypack.com>
2022-12-17 17:01:34 -0500

    Day 3 part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-static FILENAME : &'static str = "testinput.txt";
+static FILENAME : &'static str = "input.txt";
 fn main() {
     println!("Reading File: {FILENAME}\n");
     let file = File::open(FILENAME).expect("Unable to open {FILENAME}");
@@ -10,12 +10,38 @@ fn main() {
     let mut line = String::new();
     let mut len = reader.read_line(&mut line).expect("Unable to read {FILENAME}");
 
+    let mut totalPriority = 0;
     while len>2
     {
-        
+        let trim = line.trim();
+        let at = trim.len()/2;
+        let (compa, compb) = trim.split_at(at);
+        //println!( "{compa}    {compb}");
+        let common = findCommon(compa.to_string(),compb.to_string());
+        if common != '0' {
+            let pri : i32;
+            match common {
+                'a'..='z' => {pri = common as i32 - ('a' as i32) + 1},
+                'A'..='Z' => {pri = common as i32 - ('A' as i32) + 27},
+                _=> panic!("err"),
+            };
+            //println!( "common item is {common}, Priority {pri}");
+            totalPriority += pri;
+        }
         //prep next line
         line = String::new();
         len = reader.read_line(&mut line).expect("Unable to read {FILENAME}");
     }
-    //println!("Total Score: {totalscore}\n");
+    println!("Total priority: {totalPriority}\n");
+}
+
+fn findCommon( astr:String, bstr:String ) -> char{
+    for ac in astr.as_bytes().iter() {
+        for bc in bstr.as_bytes().iter(){
+            if  *ac == *bc{
+                return std::char::from_u32( *ac as u32 ).expect("err");
+            }
+        }
+    }    
+    return '0';
 }
